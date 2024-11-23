b69c0cac300546cb4d91f5f4dd84d2ebd05bd87b
dhalecod <williammakinde408@gmail.com>
2024-11-23 16:48:25 +0000

    up

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         short myNum = 12345;
         System.out.println(myNum);
     }
