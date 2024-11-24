f94ba9b4421cecaa3045f67493b5eb1c02f8b82e
dhalecod <williammakinde408@gmail.com>
2024-11-24 22:10:39 +0000

    updated

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        boolean isJavaFun = true;
-        boolean isCodingFun =true;
-        System.out.println(isJavaFun);
-        System.out.println(isCodingFun);
+        int items = 50;
+        float itemsPrice = 7.80f;
+        int totalPrice = items * itemsPrice;
+        char currency = "$";
+        System.out.println("Numbers of items :" + items);
+        System.out.println("Price for each item :" + itemsPrice + currency);
+        System.out.println("Total cost of all the items : " + totalPrice + currency);
     }
 }
