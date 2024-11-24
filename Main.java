a72edccb0437a556750eea06be012d206502cf6f
dhalecod <williammakinde408@gmail.com>
2024-11-24 22:12:59 +0000

    updated

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int items = 50;
         float itemsPrice = 7.80f;
-        int totalPrice = items * itemsPrice;
-        char currency = "$";
+        float totalPrice = items * itemsPrice;
+        char currency = '$';
         System.out.println("Numbers of items :" + items);
         System.out.println("Price for each item :" + itemsPrice + currency);
         System.out.println("Total cost of all the items : " + totalPrice + currency);
